f31d861767de9be0a8b8dd14b719d4e98342a26e
Barashov <BarashovMisha@gmail.com>
2022-06-03 21:32:38 +0300

    creating models

--- hosting/app/models.py
+++ hosting/app/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+from distutils.command import upload
+from django.db  import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Sites(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    site_photo = models.ImageField(upload_to='users_photos')
+    html_file = models.FileField(upload_to='users_files')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)    
+
+    class Meta:
+        verbose_name = 'сайт'
+        verbose_name_plural = 'сайты'
+
+    def __str__(self):
+        return self.name
+
+    
+
