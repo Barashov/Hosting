eb49131a218c56cccf74ecc413ef12733ba13dea
Barashov <BarashovMisha@gmail.com>
2022-06-03 19:03:29 +0300

    creating log in

--- hosting/hosting/urls.py
+++ hosting/hosting/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path('account/', include('account.urls')),
+    path('account/', include('django.contrib.auth.urls')),
 ]
