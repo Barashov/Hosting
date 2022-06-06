cc477b2c0fc65132d1bcdf1a45f9d97046832f98
Barashov <BarashovMisha@gmail.com>
2022-06-06 15:41:01 +0300

    create site page customization

--- hosting/app/forms.py
+++ hosting/app/forms.py
@@ -2,6 +2,15 @@ from django import forms
 from .models import Sites
 
 class CreateSiteForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'field',
+                                                         'placeholder': 'название сайта'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'field',
+                                                               'placeholder': 'описание'}))
+    site_photo = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control file-input',
+                                                                'id': 'file'}))
+    html_file = forms.FileField(widget=forms.FileInput(attrs={'accept': '.html',
+                                                              'class': 'form-control file-input',
+                                                              'id': 'html_file'}))
     
     class Meta:
         model = Sites
