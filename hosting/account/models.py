0ee019f323dabe2d9f92f34bb651c6e5c08c421c
Barashov <BarashovMisha@gmail.com>
2022-06-08 16:37:16 +0300

    creating profile

--- hosting/account/models.py
+++ hosting/account/models.py
@@ -1,3 +1,25 @@
-from django.db import models
 
+from distutils.command.upload import upload
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=14)
+    surname = models.CharField(max_length=14)
+    profile_image = models.ImageField(upload_to='profile_images/')
+    bio = models.TextField()
+    email = models.EmailField()
+    
+    
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("user", kwargs={"pk": self.pk})
+
